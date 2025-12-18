b96a09aef54e5704e0fc64a0a2666b823987aba2
Khadijah Bashir <232601918+kbashir2-ux@users.noreply.github.com>
2025-12-18 12:26:25 -0500

    Update story.py
    Past commit didn't... commit

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -40,39 +40,3 @@ if __name__ == "__main__":
 
         result = step(choice, events)
         console.print(f"[magenta]{result}[/magenta]")
-
-
-
-
-'''
-
-default_message = "You stand still, unsure what to do. The forest swallows you."
-
-def step(choice: str, events):
-    random_event = random.choice(events)
-
-    if choice == "left":
-        return left_path(random_event)
-    elif choice == "right":
-        return right_path(random_event)
-    else:
-        return default_message
-
-def left_path(event):
-    return "You walk left. " + event
-
-def right_path(event):
-    return "You walk right. " + event
-
-if __name__ == "__main__":
-    events = read_events_from_file('events.txt')
-
-    print("You wake up in a dark forest. You can go left or right.")
-    while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
-        choice = choice.strip().lower()
-        if choice == 'exit':
-            break
-        
-        print(step(choice, events))
-'''
